0f31b68ecb6f391e8caa409da6328782262e0332
Toni <arodriguezt@gmail.com>
2020-05-22 19:53:57 +0200

    Layout practices

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  height: double.infinity,
+                  width: 100.0,
+                  color: Colors.red,
+                  child: Text('Container 1'),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height:100.0,
+                      width:100,
+                      color: Colors.yellow,
+                      child: Text('Container 2'),
+                    ),
+                    Container(
+                      height:100.0,
+                      width:100,
+                      color: Colors.lime[800],
+                      child: Text('Container 2'),
+                    ),
+                  ]),
+                Container(
+                  height:double.infinity,
+                  width:100.0,
+                  color: Colors.blue,
+                  child: Text('Container 3'),
+                ),
+              ],
+            )
+          ),
+        ),
+      );
+  }
+}
+
