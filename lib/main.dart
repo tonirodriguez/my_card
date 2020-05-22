045c1acd8bdc5bf50075bc98e0a98c16aa9f978d
Toni <arodriguezt@gmail.com>
2020-05-23 01:13:13 +0200

    My Card App finished

--- lib/main.dart
+++ lib/main.dart
@@ -15,39 +15,75 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  height: double.infinity,
-                  width: 100.0,
-                  color: Colors.red,
-                  child: Text('Container 1'),
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/toni.jpg'),
                 ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height:100.0,
-                      width:100,
-                      color: Colors.yellow,
-                      child: Text('Container 2'),
+                Text(
+                  'Toni Rodriguez',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico Regular',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    color: Colors.teal[100],
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 25.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  )
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
                     ),
-                    Container(
-                      height:100.0,
-                      width:100,
-                      color: Colors.lime[800],
-                      child: Text('Container 2'),
+                    title: Text(
+                      '+34 628 374 737',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
                     ),
-                  ]),
-                Container(
-                  height:double.infinity,
-                  width:100.0,
-                  color: Colors.blue,
-                  child: Text('Container 3'),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'arodriguezt@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
                 ),
               ],
-            )
+            ),
           ),
         ),
       );
